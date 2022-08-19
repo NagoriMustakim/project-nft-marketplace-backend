fff9a956a6cf9ed3ca56e74de73fab64b1c75d26
root <root@LAPTOP-P07JSRR3.localdomain>
2022-08-19 11:23:59 +0530

    moralis complete

--- deploy/99-updateAddress.js
+++ deploy/99-updateAddress.js
@@ -1,10 +1,13 @@
 const { ethers, network } = require("hardhat")
 const fs = require("fs")
+//make sure make this in helper-hardhat-config,js
 const frontendContractFile = "../moralis-nft-marketplace/constant/networkMapping.json"
+const frontEndAbiLocation = "../moralis-nft-marketplace/constant/"
 module.exports = async function () {
     if (process.env.UPDATE_FRONT_END) {
         console.log("Updating ...")
         await updateContractAddress()
+        await updateABI()
     }
 }
 async function updateContractAddress() {
@@ -21,6 +24,18 @@ async function updateContractAddress() {
         contractAddresses[chainId] = { NFTmarketplace: [NFTmarketplace.address] }
     }
     fs.writeFileSync(frontendContractFile, JSON.stringify(contractAddresses))
+}
+async function updateABI() {
+    const NFTmarketplace = await ethers.getContract("NFTmarketplace")
+    fs.writeFileSync(
+        `${frontEndAbiLocation}NftMarketplace.json`,
+        NFTmarketplace.interface.format(ethers.utils.FormatTypes.json)
+    )
+    const basicNft = await ethers.getContract("BasicNft")
+    fs.writeFileSync(
+        `${frontEndAbiLocation}BasicNft.json`,
+        basicNft.interface.format(ethers.utils.FormatTypes.json)
+    )
 }
 // this script gonna grap our nftmarketplace contract address and export to frontend
 module.exports.tags = ["all", "updateAddress"]
